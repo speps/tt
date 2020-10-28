e1f36a28f732925684fc2cdbe02819d3407989e3
Remi Gillig <remigillig@gmail.com>
2020-10-28 08:36:49 +0000

    Replace custom atan2/pow with std version and extern version

--- runtime/wasm.d
+++ runtime/wasm.d
@@ -1,5 +1,11 @@
 module wasm;
 
-extern(C) size_t memorySize();
-extern(C) size_t growMemory(size_t by);
-extern(C) void abort();
+private:
+extern(C) size_t wasm_memorySize();
+extern(C) size_t wasm_growMemory(size_t by);
+extern(C) void wasm_abort();
+
+public:
+size_t memorySize() { return wasm_memorySize(); }
+size_t growMemory(size_t by) { return wasm_growMemory(by); }
+void abort() { wasm_abort(); }
--- src/abagames/util/math.d
+++ src/abagames/util/math.d
@@ -2,20 +2,28 @@ module abagames.util.math;
 
 version(WASM)
 {
-    extern(C) float extern_cos(float x);
-    extern(C) float extern_sin(float x);
-    extern(C) float extern_sqrt(float x);
+  private:
+  extern(C) float wasm_cos(float x);
+  extern(C) float wasm_sin(float x);
+  extern(C) float wasm_sqrt(float x);
+  extern(C) float wasm_atan2(float y, float x);
+  extern(C) float wasm_pow(float x, float y);
 
-    float cos(float x) { return extern_cos(x); }
-    float sin(float x) { return extern_sin(x); }
-    float sqrt(float x) { return extern_sqrt(x); }
+  public:
+  float cos(float x) { return wasm_cos(x); }
+  float sin(float x) { return wasm_sin(x); }
+  float sqrt(float x) { return wasm_sqrt(x); }
+  float atan2(float y, float x) { return wasm_atan2(y, x); }
+  float pow(float x, float y) { return wasm_pow(x, y); }
 }
 else
 {
-    import std.math;
-    alias cos = std.math.cos;
-    alias sin = std.math.sin;
-    alias sqrt = std.math.sqrt;
+  import std.math;
+  alias cos = std.math.cos;
+  alias sin = std.math.sin;
+  alias sqrt = std.math.sqrt;
+  alias atan2 = std.math.atan2;
+  alias pow = std.math.pow;
 }
 
 enum real PI = 0x1.921fb54442d18469898cc51701b84p+1L;
@@ -31,189 +39,189 @@ bool isNaN(float x) @nogc @trusted pure nothrow
     return x != x;
 }
 
-int signbit(float x) @nogc @trusted pure nothrow
-{
-    double dval = cast(double) x; // Precision can increase or decrease but sign won't change (even NaN).
-    return 0 > *cast(long*) &dval;
-}
+// int signbit(float x) @nogc @trusted pure nothrow
+// {
+//     double dval = cast(double) x; // Precision can increase or decrease but sign won't change (even NaN).
+//     return 0 > *cast(long*) &dval;
+// }
 
-float copysign(float to, float from) @nogc @trusted pure nothrow
-{
-  return signbit(to) == signbit(from) ? to : -to;
-}
+// float copysign(float to, float from) @nogc @trusted pure nothrow
+// {
+//   return signbit(to) == signbit(from) ? to : -to;
+// }
 
-bool isInfinity(float x) @nogc @trusted pure nothrow
-{
-    return (x < -float.max) || (float.max < x);
-}
+// bool isInfinity(float x) @nogc @trusted pure nothrow
+// {
+//     return (x < -float.max) || (float.max < x);
+// }
 
-private float poly(float x, in float[] A) @trusted pure nothrow @nogc
-{
-    uint i = A.length - 1;
-    float r = A[i];
-    while (--i >= 0)
-    {
-        r *= x;
-        r += A[i];
-    }
-    return r;
-}
+// private float poly(float x, in float[] A) @trusted pure nothrow @nogc
+// {
+//     uint i = A.length - 1;
+//     float r = A[i];
+//     while (--i >= 0)
+//     {
+//         r *= x;
+//         r += A[i];
+//     }
+//     return r;
+// }
 
-private T atanImpl(T)(T x) @safe pure nothrow @nogc
-{
-    static immutable T[4] P = [
-        -3.33329491539E-1,
-        1.99777106478E-1,
-        -1.38776856032E-1,
-        8.05374449538E-2,
-    ];
+// private T atanImpl(T)(T x) @safe pure nothrow @nogc
+// {
+//     static immutable T[4] P = [
+//         -3.33329491539E-1,
+//         1.99777106478E-1,
+//         -1.38776856032E-1,
+//         8.05374449538E-2,
+//     ];
 
-    // tan(PI/8)
-    enum T TAN_PI_8 = 0.414213562373095048801688724209698078569672L;
-    // tan(3 * PI/8)
-    enum T TAN3_PI_8 = 2.414213562373095048801688724209698078569672L;
+//     // tan(PI/8)
+//     enum T TAN_PI_8 = 0.414213562373095048801688724209698078569672L;
+//     // tan(3 * PI/8)
+//     enum T TAN3_PI_8 = 2.414213562373095048801688724209698078569672L;
 
-    // Special cases.
-    if (x == cast(T) 0.0)
-        return x;
-    if (isInfinity(x))
-        return copysign(cast(T) PI_2, x);
+//     // Special cases.
+//     if (x == cast(T) 0.0)
+//         return x;
+//     if (isInfinity(x))
+//         return copysign(cast(T) PI_2, x);
 
-    // Make argument positive but save the sign.
-    bool sign = false;
-    if (signbit(x))
-    {
-        sign = true;
-        x = -x;
-    }
+//     // Make argument positive but save the sign.
+//     bool sign = false;
+//     if (signbit(x))
+//     {
+//         sign = true;
+//         x = -x;
+//     }
 
-    // Range reduction.
-    T y;
-    if (x > TAN3_PI_8)
-    {
-        y = PI_2;
-        x = -((cast(T) 1.0) / x);
-    }
-    else if (x > TAN_PI_8)
-    {
-        y = PI_4;
-        x = (x - cast(T) 1.0)/(x + cast(T) 1.0);
-    }
-    else
-        y = 0.0;
+//     // Range reduction.
+//     T y;
+//     if (x > TAN3_PI_8)
+//     {
+//         y = PI_2;
+//         x = -((cast(T) 1.0) / x);
+//     }
+//     else if (x > TAN_PI_8)
+//     {
+//         y = PI_4;
+//         x = (x - cast(T) 1.0)/(x + cast(T) 1.0);
+//     }
+//     else
+//         y = 0.0;
 
-    // Rational form in x^^2.
-    const T z = x * x;
-    y += poly(z, P) * z * x + x;
+//     // Rational form in x^^2.
+//     const T z = x * x;
+//     y += poly(z, P) * z * x + x;
 
-    return (sign) ? -y : y;
-}
+//     return (sign) ? -y : y;
+// }
 
-float atan(float x) @safe pure nothrow @nogc
-{
-  return atanImpl(x);
-}
+// float atan(float x) @safe pure nothrow @nogc
+// {
+//   return atanImpl(x);
+// }
 
-private T atan2Impl(T)(T y, T x) @safe pure nothrow @nogc
-{
-    // Special cases.
-    if (isNaN(x) || isNaN(y))
-        return T.nan;
-    if (y == cast(T) 0.0)
-    {
-        if (x >= 0 && !signbit(x))
-            return copysign(0, y);
-        else
-            return copysign(cast(T) PI, y);
-    }
-    if (x == cast(T) 0.0)
-        return copysign(cast(T) PI_2, y);
-    if (isInfinity(x))
-    {
-        if (signbit(x))
-        {
-            if (isInfinity(y))
-                return copysign(3 * cast(T) PI_4, y);
-            else
-                return copysign(cast(T) PI, y);
-        }
-        else
-        {
-            if (isInfinity(y))
-                return copysign(cast(T) PI_4, y);
-            else
-                return copysign(cast(T) 0.0, y);
-        }
-    }
-    if (isInfinity(y))
-        return copysign(cast(T) PI_2, y);
+// private T atan2Impl(T)(T y, T x) @safe pure nothrow @nogc
+// {
+//     // Special cases.
+//     if (isNaN(x) || isNaN(y))
+//         return T.nan;
+//     if (y == cast(T) 0.0)
+//     {
+//         if (x >= 0 && !signbit(x))
+//             return copysign(0, y);
+//         else
+//             return copysign(cast(T) PI, y);
+//     }
+//     if (x == cast(T) 0.0)
+//         return copysign(cast(T) PI_2, y);
+//     if (isInfinity(x))
+//     {
+//         if (signbit(x))
+//         {
+//             if (isInfinity(y))
+//                 return copysign(3 * cast(T) PI_4, y);
+//             else
+//                 return copysign(cast(T) PI, y);
+//         }
+//         else
+//         {
+//             if (isInfinity(y))
+//                 return copysign(cast(T) PI_4, y);
+//             else
+//                 return copysign(cast(T) 0.0, y);
+//         }
+//     }
+//     if (isInfinity(y))
+//         return copysign(cast(T) PI_2, y);
 
-    // Call atan and determine the quadrant.
-    T z = atan(y / x);
+//     // Call atan and determine the quadrant.
+//     T z = atan(y / x);
 
-    if (signbit(x))
-    {
-        if (signbit(y))
-            z = z - cast(T) PI;
-        else
-            z = z + cast(T) PI;
-    }
+//     if (signbit(x))
+//     {
+//         if (signbit(y))
+//             z = z - cast(T) PI;
+//         else
+//             z = z + cast(T) PI;
+//     }
 
-    if (z == cast(T) 0.0)
-        return copysign(z, y);
+//     if (z == cast(T) 0.0)
+//         return copysign(z, y);
 
-    return z;
-}
+//     return z;
+// }
 
-float atan2(float y, float x) @safe pure nothrow @nogc
-{
-  return atan2Impl(y, x);
-}
+// float atan2(float y, float x) @safe pure nothrow @nogc
+// {
+//   return atan2Impl(y, x);
+// }
 
-float pow(float x, int n) @nogc @trusted pure nothrow
-{
-    real p = 1.0, v = void;
-    uint m = n;
-    alias n2 = n;
+// float pow(float x, int n) @nogc @trusted pure nothrow
+// {
+//     real p = 1.0, v = void;
+//     uint m = n;
+//     alias n2 = n;
 
-    if (n < 0)
-    {
-        switch (n2)
-        {
-        case -1:
-            return 1 / x;
-        case -2:
-            return 1 / (x * x);
-        default:
-        }
+//     if (n < 0)
+//     {
+//         switch (n2)
+//         {
+//         case -1:
+//             return 1 / x;
+//         case -2:
+//             return 1 / (x * x);
+//         default:
+//         }
 
-        m = cast(uint)(0 - n);
-        v = p / x;
-    }
-    else
-    {
-        switch (n)
-        {
-        case 0:
-            return 1.0;
-        case 1:
-            return x;
-        case 2:
-            return x * x;
-        default:
-        }
+//         m = cast(uint)(0 - n);
+//         v = p / x;
+//     }
+//     else
+//     {
+//         switch (n)
+//         {
+//         case 0:
+//             return 1.0;
+//         case 1:
+//             return x;
+//         case 2:
+//             return x * x;
+//         default:
+//         }
 
-        v = x;
-    }
+//         v = x;
+//     }
 
-    while (1)
-    {
-        if (m & 1)
-            p *= v;
-        m >>= 1;
-        if (!m)
-            break;
-        v *= v;
-    }
-    return p;
-}
+//     while (1)
+//     {
+//         if (m & 1)
+//             p *= v;
+//         m >>= 1;
+//         if (!m)
+//             break;
+//         v *= v;
+//     }
+//     return p;
+// }
