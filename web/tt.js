b5832ffeb0b43d6e4ddd0a242ab2eaf97ef02128
Remi Gillig <remigillig@gmail.com>
2020-12-10 10:30:26 +0000

    Fix PWA support

--- web/tt.js
+++ web/tt.js
@@ -423,8 +423,3 @@ fetch('./tt.wasm').then(function(response) {
   }
   reader.read().then(add);
 });
-
-// PWA service worker
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('./sw.js');
-}
