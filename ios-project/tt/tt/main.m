c9938425e4ffa621f532e83c242948410a200f5a
outland.karasu <outland.karasu@gmail.com>
2020-12-22 00:33:15 +0900

    issue #7 remove unused entries.

--- ios-project/tt/tt/main.m
+++ ios-project/tt/tt/main.m
@@ -1,18 +1,3 @@
-//
-//  main.m
-//  tt
-//
-//  Created by 中村暁 on 2020/12/22.
-//
-
-#import <UIKit/UIKit.h>
-#import "AppDelegate.h"
-
 int main(int argc, char * argv[]) {
-    NSString * appDelegateClassName;
-    @autoreleasepool {
-        // Setup code that might create autoreleased objects goes here.
-        appDelegateClassName = NSStringFromClass([AppDelegate class]);
-    }
-    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+    return 0;
 }
