f8a40691d865a883d84d1f2381845f9892fe30a5
outland.karasu <outland.karasu@gmail.com>
2021-01-02 14:52:18 +0900

    issue #13 fix pad position.

--- src/abagames/tt/virtualpad.d
+++ src/abagames/tt/virtualpad.d
@@ -12,67 +12,102 @@ import abagames.tt.screen : Screen;
 
 public class VirtualPad {
 
+  struct ButtonRect {
+      float x;
+      float y;
+      float width;
+      float height;
+  }
+
+  immutable WEDGES_POSITION_X = 0.0;
+  immutable WEDGES_POSITION_Y = 0.0;
+  immutable WEDGE_SIZE = 40.0;
+
+  immutable LEFT_WEDGE_RECT = ButtonRect(
+    WEDGES_POSITION_X,
+    WEDGES_POSITION_Y + WEDGE_SIZE,
+    WEDGE_SIZE,
+    WEDGE_SIZE);
+
+  immutable RIGHT_WEDGE_RECT = ButtonRect(
+    WEDGES_POSITION_X + WEDGE_SIZE * 2.0,
+    WEDGES_POSITION_Y + WEDGE_SIZE,
+    WEDGE_SIZE,
+    WEDGE_SIZE);
+
+  immutable UP_WEDGE_RECT = ButtonRect(
+    WEDGES_POSITION_X + WEDGE_SIZE,
+    WEDGES_POSITION_Y,
+    WEDGE_SIZE,
+    WEDGE_SIZE);
+
+  immutable DOWN_WEDGE_RECT = ButtonRect(
+    WEDGES_POSITION_X + WEDGE_SIZE,
+    WEDGES_POSITION_Y + WEDGE_SIZE * 2.0,
+    WEDGE_SIZE,
+    WEDGE_SIZE);
+
   void draw() {
     GL.pushMatrix();
-    GL.translate(100, 300, 0);
-    GL.scale(40.0, 40.0, 1.0);
     GL.lineWidth(4);
     GL.color(1.0, 1.0, 1.0, 1.0);
 
-    GL.pushMatrix();
-    GL.translate(-1.5, 0, 0);
     drawLeftWedge();
-    GL.popMatrix();
-
-    GL.pushMatrix();
-    GL.translate(1.5, 0, 0);
     drawRightWedge();
-    GL.popMatrix();
-
-    GL.pushMatrix();
-    GL.translate(0, -1.5, 0);
     drawUpWedge();
-    GL.popMatrix();
-
-    GL.pushMatrix();
-    GL.translate(0, 1.5, 0);
     drawDownWedge();
-    GL.popMatrix();
 
     GL.lineWidth(1);
     GL.popMatrix();
   }
 
   void drawLeftWedge() {
+    GL.pushMatrix();
+    scaling(LEFT_WEDGE_RECT);
     GL.begin(GL.LINE_LOOP);
-    GL.vertex(0, -0.5, 0);
-    GL.vertex(-0.75, 0, 0);
-    GL.vertex(0, 0.5, 0);
+    GL.vertex( 0.5,  0.5, 0);
+    GL.vertex(-0.5,  0.0, 0);
+    GL.vertex( 0.5, -0.5, 0);
     GL.end();
+    GL.popMatrix();
   }
 
   void drawUpWedge() {
+    GL.pushMatrix();
     GL.begin(GL.LINE_LOOP);
-    GL.vertex(-0.5, 0, 0);
-    GL.vertex(0, -0.75, 0);
-    GL.vertex(0.5, 0, 0);
+    scaling(UP_WEDGE_RECT);
+    GL.vertex( 0.0,  -0.5, 0);
+    GL.vertex( 0.5,   0.5, 0);
+    GL.vertex(-0.5,   0.5, 0);
     GL.end();
+    GL.popMatrix();
   }
 
   void drawRightWedge() {
+    GL.pushMatrix();
+    scaling(RIGHT_WEDGE_RECT);
     GL.begin(GL.LINE_LOOP);
-    GL.vertex(0, -0.5, 0);
-    GL.vertex(0.75, 0, 0);
-    GL.vertex(0, 0.5, 0);
+    GL.vertex(-0.5,  0.5, 0);
+    GL.vertex( 0.5,  0.0, 0);
+    GL.vertex(-0.5, -0.5, 0);
     GL.end();
+    GL.popMatrix();
   }
 
   void drawDownWedge() {
+    GL.pushMatrix();
+    scaling(DOWN_WEDGE_RECT);
     GL.begin(GL.LINE_LOOP);
-    GL.vertex(-0.5, 0, 0);
-    GL.vertex(0, 0.75, 0);
-    GL.vertex(0.5, 0, 0);
+    GL.vertex(-0.5,  -0.5, 0);
+    GL.vertex( 0.5,  -0.5, 0);
+    GL.vertex( 0.0,   0.5, 0);
     GL.end();
+    GL.popMatrix();
+  }
+
+  void scaling(ref const(ButtonRect) rect) {
+      GL.translate(rect.x + rect.width / 2.0, rect.y + rect.height / 2.0, 0);
+      GL.scale(rect.width, rect.height, 1.0);
   }
 }
 
